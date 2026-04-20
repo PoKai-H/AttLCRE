1f00498524c79936da3d5cefc93fd3d8abe7f0e0
PoKai <huangpokai31@gmail.com>
2026-04-19 23:20:25 -0500

    pretrained model implementation and dataset loader for those models

--- src/models.py
+++ src/models.py
@@ -11,3 +11,36 @@ from transformers import (
 
 
 
+class OutputWrapper:
+    """
+    Make custom model outputs look similar to Hugging Face outputs
+    """
+
+    def __init__(self, loss, logits):
+        self.loss = loss
+        self.logits = logits
+
+
+def build_model_and_tokenizer(model_name: str):
+    model_name = model_name.lower()
+
+    if model_name == "bert":
+        tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+        model = BertForSequenceClassification.from_pretrained(
+            "bert-base-uncased",
+            num_labels = 2
+        )
+        max_length = 256
+
+    elif model_name == "longformer":
+        tokenizer = LongformerTokenizer.from_predtrained("allenai/longformer-base-4096")
+        model = LongformerForSequenceClassfication.from_pretrained(
+            "allenai/longformer-base-4096",
+            num_labels = 2
+        )
+        max_length = 1024
+    
+    else:
+        raise NotImplementedError
+    
+    return model, tokenizer, max_length
