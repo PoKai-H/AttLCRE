9f787be98ef0bdc8e98440da21fc4423ee1766fb
PoKai <huangpokai31@gmail.com>
2026-05-10 16:17:38 -0500

    Add dataset files without model checkpoints

--- rm_signal.py
+++ rm_signal.py
@@ -16,10 +16,10 @@ def remove_signal(sample):
     new_sample["metadata"]["num_turns"] = len(new_sample["dialogue"])
     return new_sample
 
-with open("new_data2/test.json", "r", encoding="utf-8") as f:
+with open("data/datadata/multiwoz_generator/test/test.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 new_data = [remove_signal(sample) for sample in data]
 
-with open("new_data2/rm_signal.json", "w", encoding="utf-8") as f:
+with open("data/datadata/multiwoz_generator/rm_signal.json", "w", encoding="utf-8") as f:
     json.dump(new_data, f, indent=2, ensure_ascii=False)
